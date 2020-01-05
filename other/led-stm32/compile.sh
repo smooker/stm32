bf86004ef4443f029296c60f74cf639b112221c4
Miroslav Tzonkov <atvservice@mail.bg>
2020-01-05 14:56:58 +0200

    C Simple explained

--- other/led-stm32/compile.sh
+++ other/led-stm32/compile.sh
@@ -4,5 +4,7 @@ cat led-stm32.asm | tr "\;" "\@" > led-stm32.s
 arm-none-eabi-as -mcpu=cortex-m3 -mthumb -mapcs-32 -gstabs -ahls=led-stm32.lst -o led-stm32.o led-stm32.s
 arm-none-eabi-ld -v -T stm32.ld -nostartfiles -o led-stm32.elf led-stm32.o
 arm-none-eabi-objcopy -O binary led-stm32.elf  led-stm32.bin
+arm-none-eabi-objcopy -O ihex led-stm32.elf led-stm32.hex
+
 #stm32l.sh -V -e -w -v -p /dev/ttyUSB0 ./led-stm32.bin
 
